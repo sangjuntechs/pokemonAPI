b14cb45b98f242a8c40017511dc1684c21294b35
sangjuntechs <juntech0421@github.com>
2021-01-23 16:48:41 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [number, setNumber] = useState(0)
+
+  const carculateNumber = () => {
+    setNumber(number + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello react + typescript</p>
+      <button onClick={carculateNumber}>+</button>
+      <b>{number}</b>
     </div>
   );
 }
